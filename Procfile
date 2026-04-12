9391ca94cdc9e60b6173813ad160216faae485d5
mdeibrahim <mmeibrahim505@gmail.com>
2026-04-12 22:34:01 +0600

    final push1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn teaching_platform.wsgi --log-file -
+web: python manage.py collectstatic --noinput && gunicorn teaching_platform.wsgi --log-file -
--- teaching_platform/settings.py
+++ teaching_platform/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,6 +75,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
